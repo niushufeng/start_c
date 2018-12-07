5601801eec2dbe4b8690813595989582a4f8284d
chenboshuo <43953268+chenboshuo@users.noreply.github.com>
2018-12-08 07:45:38 +0800

    结构体的定义和简单使用
    // 结构体是将不同数据类型组织在一起的有机整体

--- structure_declaration.cpp
+++ structure_declaration.cpp
@@ -4,10 +4,8 @@ int main(int argc, char const *argv[]) {
   struct student{
     int num;
     char name[20];
-    char sex;
-    int age;
-    float scroe;
-    char addr[30];
-  };
+  }a[2] = {101,"lihua",102,"liming"};
+  printf("%d\n", a[1].num);// output 102
+  printf("%s\n", a->name);//output lihua
   return 0;
 }
