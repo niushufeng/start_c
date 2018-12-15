1d912213c8a032483c6f24c36614a4225263e285
yichuanshuzi <chenboshuo.hi@163.com>
2018-12-15 11:56:14 +0800

    输入数字分离各个位数(封装成函数)

--- sperate_each_bit_of_number.cpp
+++ sperate_each_bit_of_number.cpp
@@ -1,15 +1,27 @@
 #include "stdio.h"
 /*输入数字分离各个位数*/
 
+/**
+ * 分离数字的每一位
+ * @param  number 原始数字
+ * @param  bits   指向要存放数据的指针
+ *
+ * 最终将每一位倒序存放在 bits 中
+ */
+void every_bit(int number, int*bits){
+  do {
+    *bits = number % 10;
+    bits++;
+  } while(number/=10);
+}
+
 main(){
-  int a,x1,x2,x3;
+  int num,bits[3];
   printf("input a triple-digit number ");
   // scanf("%d", a);
-  scanf("%d", &a);
-  x1 = a / 100;
-  x2 = a / 10 % 10;
-  x3 = a % 10;
-  printf("%d %d %d", x1, x2, x3);
+  scanf("%d", &num);
+  every_bit(num, bits);
+  printf("%d %d %d", bits[2],bits[1],bits[0]);
 
 }
 //input
